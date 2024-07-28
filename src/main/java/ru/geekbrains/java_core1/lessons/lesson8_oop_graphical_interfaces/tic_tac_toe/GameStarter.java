f4dfdf998e4f2ad3c84b1dad0f02a8f9fbf9dab9
OdaryMe <elenanevsk@gmail.com>
2024-07-28 13:55:35 +0300

    Графические крестики-нолики

---
+++ src/main/java/ru/geekbrains/java_core1/lessons/lesson8_oop_graphical_interfaces/tic_tac_toe/GameStarter.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.java_core1.lessons.lesson8_oop_graphical_interfaces.tic_tac_toe;
+
+public class GameStarter {
+    public static void main(String[] args) {new GameWindow();}
+}
